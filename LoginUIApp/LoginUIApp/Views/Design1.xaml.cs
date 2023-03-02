2466c28d86131175b8258371cbfaddbdb0fe6d81
haseeb ahmad <haseeb.majoka@outlook.com>
2023-03-02 23:24:22 +0500

    first commit

---
+++ LoginUIApp/LoginUIApp/Views/Design1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginUIApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Design1 : ContentPage
+    {
+        public Design1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LoginUIApp/LoginUIApp/Views/Design2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginUIApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Design2 : ContentPage
+    {
+        public Design2()
+        {
+            InitializeComponent();
+        }
+    }
+}
